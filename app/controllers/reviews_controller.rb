7b5e7197501ee339746615ed872c0494c72e62b7
Nicolas Fan <nicfan@gmail.com>
2018-02-23 12:02:46 -0500

    Got the controllers to work

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = @review.restaurant
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to restaurant_path(@restaurant), notice: 'Review was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lloydies = Restaurant.create(name: "Lloydie's", address: "somewhere on St. Viateur", phone_number: "232132132213", category: "belgian")
+
+poutineville = Restaurant.create(name: "Poutineville", address: "somewhere on Parc", phone_number: "87787987987", category: "french")
